86f5f402ec3261f9ffe01c7d85eff0c9220ddd21
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-03-09 20:55:12 +0100

    Add .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:community_with_legends_mobile/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'screens/register_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
