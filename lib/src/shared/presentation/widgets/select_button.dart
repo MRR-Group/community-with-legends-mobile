0b193ea9b1952158116c09d50f50e638ff8fbc10
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-03-22 20:53:28 +0100

    Add SelectButton widget

---
+++ lib/src/shared/presentation/widgets/select_button.dart
@@ -0,0 +1,43 @@
+import 'package:community_with_legends_mobile/src/shared/presentation/widgets/button.dart';
+import 'package:flutter/material.dart';
+import '../../../../config/colors.dart';
+import 'loading_animation.dart';
+
+class SelectButton extends StatelessWidget {
+  const SelectButton({
+    super.key,
+    required this.text,
+    this.fontSize = 18,
+    required this.onPressed,
+    this.isSelected = false,
+  });
+
+  final String text;
+  final double? fontSize;
+  final VoidCallback onPressed;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return isSelected
+        ? Button(text: text, fontSize: fontSize, onPressed: onPressed)
+        : ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              side: BorderSide(color: primaryColor, width: 2),
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              backgroundColor: backgroundColor,
+              foregroundColor: textDisabledColor
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 28),
+              child: Text(
+                text,
+                style: TextStyle(fontSize: fontSize),
+              ),
+            ),
+          );
+  }
+}
