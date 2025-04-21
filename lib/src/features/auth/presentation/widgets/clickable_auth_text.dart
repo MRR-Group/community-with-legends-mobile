1b7a05b16838d44f6bfe1ba4a1126d2971be2f29
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-04-21 23:36:57 +0200

    Adjusted text size

--- lib/src/features/auth/presentation/widgets/clickable_auth_text.dart
+++ lib/src/features/auth/presentation/widgets/clickable_auth_text.dart
@@ -19,9 +19,12 @@ class ClickableAuthText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(
-          message,
-          style: const TextStyle(fontSize: 10),
+        SizedBox(
+          width: 120,
+          child: Text(
+            message,
+            style: const TextStyle(fontSize: 10),
+          ),
         ),
         const Spacer(),
         TextButton(
