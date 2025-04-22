9199e0512db65739e13e2db548eb662d27c493c6
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-04-22 12:23:44 +0200

    Modify User model

--- lib/src/shared/domain/models/user_model.dart
+++ lib/src/shared/domain/models/user_model.dart
@@ -2,7 +2,7 @@ class User {
   final int id;
   final String name;
   final String email;
-  final DateTime emailVerifiedAt;
+  final DateTime? emailVerifiedAt;
   final DateTime createdAt;
   final DateTime updatedAt;
 
@@ -20,7 +20,7 @@ class User {
       id: json['id'],
       name: json['name'],
       email: json['email'],
-      emailVerifiedAt: DateTime.parse(json['email_verified_at']),
+      emailVerifiedAt: DateTime.tryParse(json['email_verified_at'] ?? ''),
       createdAt: DateTime.parse(json['created_at']),
       updatedAt: DateTime.parse(json['updated_at']),
     );
